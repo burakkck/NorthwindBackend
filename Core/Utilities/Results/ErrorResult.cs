8fb9cea50635126e885f1acadaf7d2dff08f1823
burakkck <buraakkucuk@gmail.com>
2022-04-21 00:19:02 +0300

    işlem başarı ve hata sisteminin oluşturulması

---
+++ Core/Utilities/Results/ErrorResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class ErrorResult : Result
+    {
+        public ErrorResult() : base(success:true)
+        {
+        }
+
+        public ErrorResult(string message) : base(success:true, message)
+        {
+        }
+    }
+}
---
+++ Core/Utilities/Results/SuccessResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class SuccessResult : Result
+    {
+        public SuccessResult() : base(success: true)
+        {
+
+        }
+        public SuccessResult(string message) : base(success:true, message)
+        {
+
+        }
+        
+
+        
+    }
+}
